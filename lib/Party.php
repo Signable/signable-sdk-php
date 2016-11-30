4ae827355d732d6ff500af9443e301c4961afbd2
Fizdalf <fizdalf@gmail.com>
2016-11-30 17:13:11 +0000

    - Add in-person signing flag.

--- lib/Party.php
+++ lib/Party.php
@@ -13,23 +13,26 @@ class Party {
     public $party_id;
     public $party_role;
     public $party_message;
+    public $signature_in_person;
 
     /**
      * Create the party object using the parameters provided
      *
      * If no template id used party ID is not required and a role must be provided instead
      *
-     * @param string $partyName    A string of the name of the signing party
-     * @param string $partyEmail   A string of a valid email address of the party
-     * @param int    $partyID      An integer representing the ID of the party which is setup in the template that you want to send out.
-     * @param string $partyMessage A message which will be sent to this party when it is their turn to sign. It is shown in the email that is sent to the signing party.
-     * @param string $haveTemplate 'yes' or 'no' dependant on whether a template is being used
+     * @param string $partyName       A string of the name of the signing party
+     * @param string $partyEmail      A string of a valid email address of the party
+     * @param int    $partyID         An integer representing the ID of the party which is setup in the template that you want to send out.
+     * @param string $partyMessage    A message which will be sent to this party when it is their turn to sign. It is shown in the email that is sent to the signing party.
+     * @param string $haveTemplate    'yes' or 'no' dependant on whether a template is being used
+     * @param bool   $inPersonSigning (optional) true if this party should sign in-person, default false.
      */
-    public function __construct( $partyName, $partyEmail, $partyID, $partyMessage = '',  $haveTemplate = 'yes' ) {
+    public function __construct( $partyName, $partyEmail, $partyID, $partyMessage = '',  $haveTemplate = 'yes', $inPersonSigning = false ) {
 
-        $this->party_name    = $partyName;
-        $this->party_email   = $partyEmail;
-        $this->party_message = $partyMessage;
+        $this->party_name              = $partyName;
+        $this->party_email             = $partyEmail;
+        $this->party_message           = $partyMessage;
+        $this->signature_in_person = $inPersonSigning;
 
         if ( 'yes' == $haveTemplate ) {
             $this->party_id   = $partyID;
